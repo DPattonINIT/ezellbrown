f75d03878c918b4ab3257e30ef324c8cf1be99ed
DPattonINIT <DlamarPatton@gmail.com>
2025-06-04 13:57:11 -0700

    updated

---
+++ pages/booking.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
+const BookingPage = () => {
+  return (
+    <div>
+        <Sidebar />
+    <div className="p-8 text-white">
+      <h1 className="text-3xl">BOOKING PAGE</h1>
+      <p>Welcome to the Booking page.</p>
+    </div>
+    </div>
+  );
+};
+
+export default BookingPage;
---
+++ pages/events.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
+const EventsPage = () => {
+  return (
+    <div>
+        <Sidebar />
+    <div className="p-8 text-white">
+      <h1 className="text-3xl">EVENTS PAGE</h1>
+      <p>Welcome to the Events page.</p>
+    </div>
+    </div>
+  );
+};
+
+export default EventsPage;
